913d1a2abd26d7c5222fd40daaca035c52b08e6e
sebasmedinaarg <146899525+sebasmedinaarg@users.noreply.github.com>
2023-11-13 12:05:09 -0600

    Add files via upload

---
+++ Parte1/act12.js
@@ -0,0 +1,17 @@
+// Declarar e inicializar una variable `let` dentro de un bloque
+{
+    let miVariableLet = "Hola Mi nombre es Sebastain";
+    console.log(miVariableLet); // "Hola Mi nombre es Sebastain"
+  }
+  
+  // Declarar e inicializar una variable `const` dentro de un bloque
+  {
+    const miVariableConst = "Esto es una constante";
+    console.log(miVariableConst); // "Esto es una constante"
+  }
+  
+  // Declarar e inicializar una variable `var` dentro de un bloque
+  {
+    var miVariableVar = "Aqui se puede cambiar el valor";
+    console.log(miVariableVar); // "Aqui se puede cambiar el valor"
+  }
